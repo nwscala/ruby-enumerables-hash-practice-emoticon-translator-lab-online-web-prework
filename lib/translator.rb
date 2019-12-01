3533bcbaf29feaf8201207842083b8565272c8fe
Noah Scala <tobi8912@gmail.com>
2019-12-01 20:24:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,11 @@ require"yaml" # require modules here
 
 def load_library(filepath)
   # code goes here
-  YAML.load_file(filepath)
+  emoticons = YAML.load_file(filepath)
   
   emoticon_lib = {:get_meaning => {}, :get_emoticon => {}}
   
-  emoticon_lib.each do 
+  emoticons.each do |meaning, value|
 end
 
 def get_japanese_emoticon
