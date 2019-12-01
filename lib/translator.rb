09e126990a55ca8658e59f4289229753decc2cde
Noah Scala <tobi8912@gmail.com>
2019-12-01 20:31:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 def get_japanese_emoticon(filepath, english_emoticon)
   # code goes here
   emoticon_lib = load_library(filepath)
-  
+  if emoticon_lib.include?
 end
 
 def get_english_meaning
