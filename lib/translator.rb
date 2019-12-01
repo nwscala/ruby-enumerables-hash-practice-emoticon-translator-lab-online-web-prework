06a058447daaf5199a46212cfdcea2af9c50a726
Noah Scala <tobi8912@gmail.com>
2019-12-01 20:20:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(filepath)
   # code goes here
   YAML.load_file(filepath)
   
-  emoticon_lib = {:get_meaning => [], :get_emoticon => []}
+  emoticon_lib = {:get_meaning => {}, :get_emoticon => {}}
 end
 
 def get_japanese_emoticon
